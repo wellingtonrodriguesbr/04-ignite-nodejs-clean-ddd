354cbbc85834d57497fda2e99b27564c9676650b
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-10-13 16:20:36 -0300

    chore: mapping props to answer and question

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,10 +1,14 @@
 import { Slug } from "./value-objects/slug";
 import { Entity } from "../../core/entities/entity";
+import { UniqueEntityId } from "../../core/entities/unique-entity-id";
 
 interface QuestionProps {
+  authorId: UniqueEntityId;
+  bestAnswerId?: UniqueEntityId;
   title: string;
   slug: Slug;
   content: string;
-  authorId: string;
+  createAt: Date;
+  updateAt?: Date;
 }
 export class Question extends Entity<QuestionProps> {}
