b89106a741d2f5f6eb3feda8be94f463bd1424e3
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-10-15 09:52:49 -0300

    chore: generating fictitious data

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -4,15 +4,22 @@ import {
   QuestionProps,
 } from "@/domain/forum/enterprise/entities/question";
 import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
+import { faker } from "@faker-js/faker";
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityId(),
-    title: "Example title",
-    content: "Example Content",
-    slug: Slug.create("example-title"),
-    ...override,
-  });
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityId
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityId(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      slug: Slug.create("example-title"),
+      ...override,
+    },
+    id
+  );
 
   return question;
 }
