4f399573d85e3310aca43e0a038a993a6f5df586
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-10-16 06:19:30 -0300

    test: test delete answer use case

---
+++ src/domain/forum/application/use-cases/delete-answer.spec.ts
@@ -0,0 +1,48 @@
+import { InMemoryAnswersRepository } from "test/repositories/in-memory-answers-repository";
+import { DeleteAnswerUseCase } from "./delete-answer";
+import { makeAnswer } from "test/factories/make-answer";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository;
+let sut: DeleteAnswerUseCase;
+
+describe("Delete Answer", () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository();
+    sut = new DeleteAnswerUseCase(inMemoryAnswersRepository);
+  });
+
+  it("should be able to delete a answer", async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityId("author-1"),
+      },
+      new UniqueEntityId("answer-1")
+    );
+    await inMemoryAnswersRepository.create(newAnswer);
+
+    await sut.execute({
+      answerId: "answer-1",
+      authorId: "author-1",
+    });
+
+    expect(inMemoryAnswersRepository.items).toHaveLength(0);
+  });
+
+  it("should not be able to delete a answer from another author", async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityId("author-1"),
+      },
+      new UniqueEntityId("answer-1")
+    );
+    await inMemoryAnswersRepository.create(newAnswer);
+
+    expect(() => {
+      return sut.execute({
+        answerId: "answer-1",
+        authorId: "author-2",
+      });
+    }).rejects.toBeInstanceOf(Error);
+  });
+});
--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -3,7 +3,23 @@ import { Answer } from "@/domain/forum/enterprise/entities/answer";
 
 export class InMemoryAnswersRepository implements AnswersRepository {
   public items: Answer[] = [];
+
   async create(answer: Answer) {
     this.items.push(answer);
+  }
+
+  async findById(answerId: string) {
+    const answer = this.items.find((item) => item.id.toString() === answerId);
+
+    if (!answer) {
+      return null;
+    }
+
+    return answer;
+  }
+
+  async delete(answer: Answer) {
+    const answerIndex = this.items.findIndex((q) => q.id === answer.id);
+    this.items.splice(answerIndex, 1);
   }
 }
