3b30d852f8987357527375ac0af3d3e9b5e5ffa3
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-10-22 10:01:00 -0300

    chore: remove unused imports

--- src/domain/forum/application/use-cases/choose-question-best-answer.ts
+++ src/domain/forum/application/use-cases/choose-question-best-answer.ts
@@ -1,6 +1,4 @@
-import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { AnswersRepository } from "@/domain/forum/application/repositories/answers-repository";
-import { Answer } from "../../enterprise/entities/answer";
 import { Question } from "../../enterprise/entities/question";
 import { QuestionsRepository } from "../repositories/questions-repository";
 
