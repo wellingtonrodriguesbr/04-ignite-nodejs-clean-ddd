a90ccd9b27a84bfc375450b133996da3a3a84ae3
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-10-25 08:42:47 -0300

    feat: create answer and question comment entities

---
+++ src/domain/forum/enterprise/entities/answer-comment.ts
@@ -0,0 +1,50 @@
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { Optional } from "@/core/types/optional";
+
+export interface AnswerCommentProps {
+  authorId: UniqueEntityId;
+  answerId: UniqueEntityId;
+  content: string;
+  createdAt: Date;
+  updatedAt?: Date;
+}
+
+export class AnswerComment extends Entity<AnswerCommentProps> {
+  get content() {
+    return this.props.content;
+  }
+
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  private touch() {
+    return (this.props.updatedAt = new Date());
+  }
+
+  set content(content: string) {
+    this.props.content = content;
+    this.touch();
+  }
+
+  static create(
+    props: Optional<AnswerCommentProps, "createdAt">,
+    id?: UniqueEntityId
+  ) {
+    const answerComment = new AnswerComment(
+      { ...props, createdAt: props.createdAt ?? new Date() },
+      id
+    );
+
+    return answerComment;
+  }
+}
---
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -0,0 +1,50 @@
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { Optional } from "@/core/types/optional";
+
+export interface QuestionCommentProps {
+  authorId: UniqueEntityId;
+  questionId: UniqueEntityId;
+  content: string;
+  createdAt: Date;
+  updatedAt?: Date;
+}
+
+export class QuestionComment extends Entity<QuestionCommentProps> {
+  get content() {
+    return this.props.content;
+  }
+
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  private touch() {
+    return (this.props.updatedAt = new Date());
+  }
+
+  set content(content: string) {
+    this.props.content = content;
+    this.touch();
+  }
+
+  static create(
+    props: Optional<QuestionCommentProps, "createdAt">,
+    id?: UniqueEntityId
+  ) {
+    const questionComment = new QuestionComment(
+      { ...props, createdAt: props.createdAt ?? new Date() },
+      id
+    );
+
+    return questionComment;
+  }
+}
