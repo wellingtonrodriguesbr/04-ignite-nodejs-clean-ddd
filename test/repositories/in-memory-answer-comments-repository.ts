4e896d5b3d51896ad1d431137f34a3e90423206b
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-12-10 07:34:42 -0300

    feat: create answer comments listing

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { AnswerCommentsRepository } from "@/domain/forum/application/repositories/answer-comments-repository";
 import { AnswerComment } from "@/domain/forum/enterprise/entities/answer-comment";
 
@@ -16,6 +17,13 @@ export class InMemoryAnswerCommentsRepository
     return answerComment;
   }
 
+  async findManyByAnswerId(answerId: string, { page }: PaginationParams) {
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20);
+
+    return answerComments;
+  }
   async create(answerComment: AnswerComment) {
     this.items.push(answerComment);
   }
