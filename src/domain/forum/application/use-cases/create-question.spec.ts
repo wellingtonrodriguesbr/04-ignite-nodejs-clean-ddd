24b811f85eab824bb3e588864c8cc517e32e5a0f
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-01-14 09:09:04 -0300

    test: test create question with attachments

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { CreateQuestionUseCase } from "./create-question";
 import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
 
@@ -15,11 +16,17 @@ describe("Create Question", () => {
       authorId: "1",
       title: "title of question",
       content: "content of question",
+      attachmentsIds: ["1", "2"],
     });
 
     expect(result.isRight()).toBeTruthy();
     expect(inMemoryQuestionsRepository.items[0]).toEqual(
       result.value?.question
     );
+    expect(inMemoryQuestionsRepository.items[0].attachments).toHaveLength(2);
+    expect(inMemoryQuestionsRepository.items[0].attachments).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityId("1") }),
+      expect.objectContaining({ attachmentId: new UniqueEntityId("2") }),
+    ]);
   });
 });
